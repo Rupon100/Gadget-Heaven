91d2b752ad041a75d3c746dc4010f404fa8030a2
Rupon <fardiidlamrupon@gmail.com>
2024-11-04 16:16:13 +0600

    load data successfully

---
+++ src/Gadgets/Gadget.jsx
@@ -0,0 +1,10 @@
+ 
+const Gadget = () => {
+    return (
+        <div>
+            <h1>Gadget</h1>
+        </div>
+    );
+};
+
+export default Gadget;
--- src/Gadgets/Gadgets.jsx
+++ src/Gadgets/Gadgets.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Gadget from "./Gadget";
+
  
 
 const Gadgets = () => {
+    const [gadgets, setGadgets] = useState([]);
+    useEffect(() => {
+        fetch('./data.json')
+        .then(response => response.json())
+        .then(data => setGadgets(data))
+    } ,[])
     return (
         <div>
             <h2>Gadgets data</h2>
+            <p>{gadgets.length}</p>
+            <Gadget></Gadget>
         </div>
     );
 };
