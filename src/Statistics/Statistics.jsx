21df82efa13e9bdc52ab473b0783a38c3d9aad0b
Rupon <fardiidlamrupon@gmail.com>
2024-11-06 09:41:54 +0600

    statistics section completed

--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,9 +1,86 @@
- 
+import React, { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+
     return (
         <div>
-            <h1>Stastatics here</h1>
+            <div className="bg-purple-600 text-white p-10 text-center space-y-4">
+                <h2 className="text-4xl font-semibold">Statistics</h2>
+                <p className="text-gray-200 w-4/5 mx-auto">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
+            </div>
+            <div className="max-w-5xl mx-auto p-6 space-y-4">
+               <h2 className="text-2xl font-semibold">Statistics</h2>
+              
+               <div style={{ width: '100%' }} className='p-6'>
+                        <ResponsiveContainer width="100%" height={200}>
+                          <AreaChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            syncId="anyId"
+                            margin={{
+                              top: 10,
+                              right: 30,
+                              left: 0,
+                              bottom: 0,
+                            }}
+                          >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                          </AreaChart>
+                        </ResponsiveContainer>
+                       
+                    </div>
+               </div>
         </div>
     );
 };
