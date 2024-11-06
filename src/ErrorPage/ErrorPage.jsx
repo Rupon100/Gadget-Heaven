ef53a4582c17d365405d92d61c3a8cb63d87d77e
Rupon <fardiidlamrupon@gmail.com>
2024-11-06 11:30:52 +0600

    error page more functionality added

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
  
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1 className="text-4xl text-center">No Data Found!</h1>
+        <div className="text-center">
+            <h1 className="text-4xl text-center p-10">No Data Found!</h1>
+            <Link to="/" className="border p-2 rounded-md">
+               go back go home
+            </Link>
         </div>
     );
 };
